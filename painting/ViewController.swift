9bfcca27a961141abcf5f24faf7d8e3783a1ee3a
Shravan Kumaran <shravan.sat@gmail.com>
2020-02-29 13:52:05 -0600

    done

--- painting/ViewController.swift
+++ painting/ViewController.swift
@@ -17,31 +17,37 @@ class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDele
     
     private var locationManager: CLLocationManager!
     private var currentLocation: CLLocation?
-    var swiped = false
+    var swipedUp = false
+    var swipedRight = false
+    
     @objc func respondToSwipeGesture(gesture: UIGestureRecognizer) {
-            if let swipeGesture = gesture as? UISwipeGestureRecognizer {
-                switch swipeGesture.direction {
-                    case UISwipeGestureRecognizer.Direction.down:
-                        print("Swiped down")
-                        swiped = false
-                        self.viewDidLoad()
-//                    case UISwipeGestureRecognizer.Direction.right:
-//                        print("Swiped right")
-//                        swiped = false
-//                        self.viewDidLoad()
-//                    case UISwipeGestureRecognizer.Direction.left:
-//                        print("Swiped left")
-//                        swiped = false
-//                        self.viewDidLoad()
-                    case UISwipeGestureRecognizer.Direction.up:
-                        print("Swiped up")
-                        swiped = true
-                        self.viewDidLoad()
-                    default:
-                        break
-                }
+        if let swipeGesture = gesture as? UISwipeGestureRecognizer {
+            switch swipeGesture.direction {
+                case UISwipeGestureRecognizer.Direction.down:
+                    print("swipedUp down")
+                    swipedUp = false
+                    swipedRight = false
+                    self.viewDidLoad()
+                case UISwipeGestureRecognizer.Direction.right:
+                    print("swipedUp right")
+                    swipedUp = false
+                    swipedRight = true
+                    self.viewDidLoad()
+                case UISwipeGestureRecognizer.Direction.up:
+                    print("swipedUp up")
+                    swipedUp = true
+                    swipedRight = false
+                    self.viewDidLoad()
+                default:
+                    break
             }
         }
+    }
+    
+    @IBAction func didTapGoogle(sender: AnyObject) {
+        UIApplication.shared.open(URL(string: "www.google.com")!, options: [:], completionHandler: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -54,8 +60,53 @@ class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDele
         swipeDown.direction = UISwipeGestureRecognizer.Direction.down
         self.view.addGestureRecognizer(swipeDown)
         
+        let scanningPanel = UIImageView()
+        scanningPanel.backgroundColor = UIColor(displayP3Red: 0.501, green: 0.0, blue: 0.0, alpha: 1.0)
+        scanningPanel.layer.masksToBounds = true
+        scanningPanel.frame = CGRect(x: self.arView.frame.width/2-90,
+                                     y: 40,
+                                 width: 180,
+                                height: 50)
+
+        scanningPanel.layer.cornerRadius = 10
+
+        let scanInfo = UILabel(frame: CGRect(x: self.arView.frame.width/2-80,
+                                             y: 40,
+                                         width: 160,
+                                        height: 50))
+
+        scanInfo.textAlignment = .left
+        scanInfo.font = scanInfo.font.withSize(20)
+        scanInfo.textColor = UIColor.white
+        scanInfo.text = "UNSUNG AGGIES"
+        scanInfo.font = UIFont(name:"Avenir Next", size: 18.0)
+        self.arView.addSubview(scanningPanel)
+        self.arView.addSubview(scanInfo)
         
-        if(swiped == true){
+        
+        let btnBack = UIImageView()
+        btnBack.backgroundColor = UIColor(displayP3Red: 0.0, green: 0.2, blue: 0.8, alpha: 1.0)
+        btnBack.layer.masksToBounds = true
+        btnBack.frame = CGRect(x: self.arView.frame.width-100,
+             y: 40,
+         width: 80,
+        height: 50)
+
+        btnBack.layer.cornerRadius = 10
+        
+        let donateBtn = UIButton(frame: CGRect(x: self.arView.frame.width-120,
+                                                   y: 40,
+                                               width: 120,
+                                              height: 50))
+        donateBtn.setTitle("Donate!", for: .normal)
+        
+        donateBtn.addTarget(self, action: "didTapGoogle", for: .touchUpInside)
+        
+        self.arView.addSubview(btnBack)
+        self.arView.addSubview(donateBtn)
+        
+        
+        if(swipedUp == true){
             let pos = CGRect(x: 10,
                 y: 2*arView.frame.height/3,
                 width: self.arView.frame.width-20,
@@ -67,7 +118,9 @@ class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDele
             mapView.layer.cornerRadius = 10
             
             mapView.showsUserLocation = true
+            mapView.mapType = MKMapType.satellite
         
+            
             mapView.delegate = self
             mapView.showsUserLocation = true
             locationManager = CLLocationManager()
@@ -85,12 +138,25 @@ class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDele
                 if currentLocation == nil {
                     // Zoom to user location
                     if let userLocation = locations.last {
-                        let viewRegion = MKCoordinateRegion(center: userLocation.coordinate, latitudinalMeters: 1
-                            , longitudinalMeters: 1)
+                        let viewRegion = MKCoordinateRegion(center: userLocation.coordinate, latitudinalMeters: 2
+                            , longitudinalMeters: 2)
                         mapView.setRegion(viewRegion, animated: false)
                     }
                 }
             }
+            
+            var annotation = MKPointAnnotation()
+            var centerCoordinate = CLLocationCoordinate2D(latitude: 30.614298, longitude:-96.339449)
+            annotation.coordinate = centerCoordinate
+            annotation.title = "Corps Of Cadets"
+            mapView.addAnnotation(annotation)
+           
+            annotation = MKPointAnnotation()
+            centerCoordinate = CLLocationCoordinate2D(latitude: 30.619734, longitude:-96.338191)
+            annotation.coordinate = centerCoordinate
+            annotation.title = "Langford A Architecture"
+            mapView.addAnnotation(annotation)
+            
             self.arView.addSubview(mapView)
 
         }
@@ -99,10 +165,43 @@ class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDele
                 if v is MKMapView{
                    v.removeFromSuperview()
                 }
-                
             }
         }
-        // Add the box anchor to the scene
+        
+//        if(swipedRight == true){
+//            let scanningPanel = UIImageView()
+//           scanningPanel.backgroundColor = UIColor(white: 0.33, alpha: 0.6)
+//           scanningPanel.layer.masksToBounds = true
+//           scanningPanel.frame = CGRect(x: 20,
+//                                       y: 40,
+//                                       width: self.arView.frame.width-20,
+//                                       height: 30)
+//
+//
+//           scanningPanel.layer.cornerRadius = 10
+//
+//           let scanInfo = UILabel(frame: CGRect(x: 20,
+//                                                y: 40,
+//                                            width: self.arView.frame.width-20,
+//                                            height: 30))
+//
+//           scanInfo.textAlignment = .left
+//           scanInfo.font = scanInfo.font.withSize(15)
+//           scanInfo.textColor = UIColor.white
+//           scanInfo.text = "* She is the first African American women to have joined the Corps of Cadets."
+//           self.arView.addSubview(scanningPanel)
+//           self.arView.addSubview(scanInfo)
+//        }
+//        else{
+//            for v in self.arView.subviews {
+//                if v is UIImageView{
+//                   v.removeFromSuperview()
+//                }
+//                if v is UILabel{
+//                   v.removeFromSuperview()
+//                }
+//            }
+//        }
         
     }
     override func viewDidAppear(_ animated: Bool) {
