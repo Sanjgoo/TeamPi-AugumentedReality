3b750c9212098b2b1f669cbf570314c95339074d
Shravan Kumaran <shravan.sat@gmail.com>
2020-02-29 01:05:35 -0600

    working maps

--- painting/ViewController.swift
+++ painting/ViewController.swift
@@ -8,17 +8,57 @@
 
 import UIKit
 import RealityKit
+import MapKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
     
     @IBOutlet var arView: ARView!
+    
+    private var locationManager: CLLocationManager!
+    private var currentLocation: CLLocation?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Load the "Box" scene from the "Experience" Reality File
-        let boxAnchor = try! Experience.loadBox()
+        let pos = CGRect(x: 10,
+            y: 2*arView.frame.height/3,
+            width: self.arView.frame.width-20,
+            height: self.arView.frame.height/3-10)
         
+        let mapView = MKMapView(frame: pos)
+        
+        mapView.layer.cornerRadius = 10.0;
+        mapView.layer.cornerRadius = 10
+        let boxAnchor = try! Experience.loadBox()
+        mapView.showsUserLocation = true
+    
+        
+        mapView.delegate = self
+        mapView.showsUserLocation = true
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+
+        // Check for Location Services
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.requestWhenInUseAuthorization()
+            locationManager.startUpdatingLocation()
+        }
+        func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+            defer { currentLocation = locations.last }
+
+            if currentLocation == nil {
+                // Zoom to user location
+                if let userLocation = locations.last {
+                    let viewRegion = MKCoordinateRegion(center: userLocation.coordinate, latitudinalMeters: 1
+                        , longitudinalMeters: 1)
+                    mapView.setRegion(viewRegion, animated: false)
+                }
+            }
+        }
+        self.arView.addSubview(mapView)
+
         // Add the box anchor to the scene
         arView.scene.anchors.append(boxAnchor)
     }
