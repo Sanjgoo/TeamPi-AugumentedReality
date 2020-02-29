b3f69d0ff236561142b31684f5eb0caa42cac355
sanjaykumaran <sanjbanz@gmail.com>
2020-02-28 22:23:55 -0600

    Initial commit

---
+++ painting/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  painting
+//
+//  Created by Sanjay Kumaran on 2/28/20.
+//  Copyright © 2020 Sanjay Kumaran. All rights reserved.
+//
+
+import UIKit
+import RealityKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var arView: ARView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+    }
+}
